395b920a220dd35da6d27a363c779745e1cca4ef
Ioana <99274396+Ioana-Mar@users.noreply.github.com>
2022-03-22 21:52:29 +0000

    added to index.html, created and made stylesheet, created js function

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Created by: Ioana Marinescu
+// Created on: Mar 22 2022
+// This file contains the JS functions for index.html
+
+function calcAreaPer () {
+  
+  // area and perimeter calculator function
+  document.getElementById('calc-area').innerHTML = 'The area is: 20 * 32 = ' + (20 * 32) + 'cm^2'
+	document.getElementById('calc-per').innerHTML = 'The perimeter is: 20 * 2 + 32 * 2 = ' + (2*20 + 2*32) + 'cm'
+}
